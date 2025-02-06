1b5e76f932ecb5ba567885fa1291610bc750d6a4
Max2000321 <shivikovmaxim@gmail.com>
2025-02-06 11:51:13 +0200

    Delete files directory

--- files/backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/contact', (req, res) => {
-  res.json({ phone: '+3800000000', instagram: 'https://instagram.com/yourclinic' });
-});
-
-module.exports = router;
